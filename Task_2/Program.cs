74820644fe3ef6a3a2ba25bbcfc234d62d26ae9e
Victoria <victoria.kainova@gmail.com>
2022-10-02 22:00:44 +0300

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое целое число: ");
+string userInput1 = Console.ReadLine() ??"";
+int a = int.Parse (userInput1);
+Console.Write("Введите второе целое число: ");
+string userInput2 = Console.ReadLine() ??"";
+int b = int.Parse (userInput2);
+if (a > b)
+{
+    Console.Write("max = ");
+    Console.Write(a);
+}
+else
+{
+   Console.Write("max = ");
+   Console.Write(b);
+}
